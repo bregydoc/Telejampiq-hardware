164b02583f76e865a272383acb7ff81d6de95ad9
bregydoc <bregy.malpartida@utec.edu.pe>
2016-06-30 17:06:58 -0500

    version 0.0.3, ok status

--- telejamíq_on_raspberry/main.py
+++ telejamíq_on_raspberry/main.py
@@ -3,8 +3,10 @@ import time
 import os
 import random
 import commands
+import sys
 
 #from control_led import HardwareLedRpi
+from simpleControlLed import SimpleHardwareLeds
 from helperDB import HelperDB
 
 def scanAvailableUSBDisp():
@@ -15,12 +17,44 @@ def scanAvailableUSBDisp():
 
 hdb = HelperDB()
 pulsoximeter = cms50dplus.CMS50Dplus(scanAvailableUSBDisp())
+sLeds = SimpleHardwareLeds()
 pulsoximeter.connect()
 
+sLeds.onBusy()
 
 
+i = 0
+def mainLoop():
+    global i
+    i+=1
+    print "Iteracion numero " + str(i)
+    try:
+        while 1:
+            sLeds.onBusy()
+            r = pulsoximeter.getLiveData().next()
+            sLeds.onOK()
+            bSpO2 = r.bloodSpO2
+            pulse = r.pulseRate
+            if (not pulse==0) and (not bSpO2==0):
+                currentTime = r.time
+                requestPost = hdb.uploadDataToTelejampiqDB([bSpO2, pulse, hdb.parseDateString(time.strftime("%c"))])
+                if requestPost.status_code==200 or requestPost.status_code==201:
+                    print "UPLOAD OK"
+                else:
+                    print "UPLOAD FAILED"
+            time.sleep(0.5)
+    except Exception as e:
+        sLeds.onError()
+        time.sleep(0.2)
+        mainLoop()
+            print e
 
 
+mainLoop()
+
+
+"""
+sLeds.onError()
 while 1:
     resp = pulsoximeter.getLiveData().next()
     rBloodSpO2 = resp.bloodSpO2
@@ -28,6 +62,7 @@ while 1:
     print hdb.uploadDataToTelejampiqDB([rBloodSpO2, rPulseRate, hdb.parseDateString(time.strftime("%c"))])
     time.sleep(0.5)
 
+"""
 """
 hdb = HelperDB()
 
--- telejamíq_on_raspberry/simpleControlLed.py
+++ telejamíq_on_raspberry/simpleControlLed.py
@@ -1,3 +1,48 @@
 import RPi.GPIO as GPIO
+from threading import *
 
-class HardwareLedRpi:
+class SimpleHardwareLeds:
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+
+        self.redLed = 35
+        self.greenLed = 33
+        self.blueLed = 32
+        self.ledsArray = [self.redLed, self.greenLed, self.blueLed]
+
+        GPIO.setup(self.redLed, GPIO.OUT)
+        GPIO.setup(self.greenLed, GPIO.OUT)
+        GPIO.setup(self.blueLed, GPIO.OUT)
+
+
+
+    def digitalWrite(self, pin, state = 1):
+        if state==1:
+            GPIO.output(pin, GPIO.HIGH)
+        elif state==0:
+            GPIO.output(pin, GPIO.LOW)
+        else:
+            return -1
+
+    def onBusy(self):
+        for i in range(len(self.ledsArray)):
+            self.digitalWrite(self.ledsArray[i], 1)
+        self.digitalWrite(self.blueLed, 0)
+
+    def onError(self):
+        for i in range(len(self.ledsArray)):
+            self.digitalWrite(self.ledsArray[i], 1)
+        self.digitalWrite(self.redLed, 0)
+
+    def onOK(self):
+        for i in range(len(self.ledsArray)):
+            self.digitalWrite(self.ledsArray[i], 1)
+        self.digitalWrite(self.greenLed, 0)
+
+    def stopBusy(self):
+        self.digitalWrite(self.blueLed, 1)
+    def stopError(self):
+        self.digitalWrite(self.redLed, 1)
+    def noOk(self):
+        self.digitalWrite(self.greenLed, 1)
