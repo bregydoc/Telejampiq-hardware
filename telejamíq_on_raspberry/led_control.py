41a208663e91b6141af577d9d0b869eac527e853
Bregy Malpartida Ramos <bregy.malpartida@utec.edu.pe>
2016-06-30 04:40:11 -0500

    The first commit

---
+++ telejamíq_on_raspberry/led_control.py
@@ -0,0 +1,139 @@
+import RPi.GPIO as GPIO
+import time
+from sys import exit
+
+from threading import Thread
+
+class HardwareLedRpi:
+    def __init__(self):
+
+        GPIO.setmode(GPIO.BOARD)
+	GPIO.setwarnings(False)
+
+        GPIO.setup(32, GPIO.OUT)
+        GPIO.setup(33, GPIO.OUT)
+        GPIO.setup(35, GPIO.OUT)
+
+        self.blue = GPIO.PWM(32, 100)
+        self.green = GPIO.PWM(33, 100)
+        self.red = GPIO.PWM(35, 100)
+
+	self.blue.start(100.0)
+        self.green.start(100.0)
+        self.red.start(100.0)
+
+
+	
+	self.onBusyFlag = False
+	self.onErrorFlag = False
+	
+
+        
+
+	self.busyThread = Thread(target=self.busy)
+	self.busyThread.start()
+
+	self.errorThread = Thread(target=self.error)
+	self.errorThread.start()
+
+
+
+    def startAll(self):
+	self.blue.start(100.0)
+        self.green.start(100.0)
+        self.red.start(100.0)	
+		
+    def cleanAll(self):
+	GPIO.cleanup(32)
+	GPIO.cleanup(35)
+	GPIO.cleanup(33)
+	
+	self.red.stop()
+	self.blue.stop()
+	self.green.stop()
+	
+
+    def busy(self):
+    	vel = 1
+	while True:
+		while self.onBusyFlag:
+		        for i in range(0,100, vel):
+		            self.blue.ChangeDutyCycle(i)
+		            time.sleep(0.009)
+			    
+	
+			time.sleep(0.1)
+
+
+		        for i in range(100, 0, -1*vel):
+		            self.blue.ChangeDutyCycle(i)
+		            time.sleep(0.009)
+		            
+		while not self.onBusyFlag:
+			time.sleep(0.1)
+
+
+    def error(self):
+	vel = 1
+	while True:
+		while self.onErrorFlag:
+		        for i in range(0,100, vel):
+		            self.red.ChangeDutyCycle(i)
+		            time.sleep(0.009)
+			    
+	
+			time.sleep(0.1)
+
+
+		        for i in range(100, 0, -1*vel):
+		            self.red.ChangeDutyCycle(i)
+		            time.sleep(0.009)
+		            
+		while not self.onErrorFlag:
+			time.sleep(0.1)
+		 
+
+    def onBusy(self):
+	self.blue.start(100.0)
+	self.onBusyFlag = True    
+
+    def stopBusy(self):	
+	self.onBusyFlag = False
+	#self.blue.ChangeDutyCycle(100.0)
+	self.blue.stop()
+	GPIO.cleanup(32)
+	#self.cleanAll()
+
+    def onError(self):
+
+	self.red.start(100.0)
+	self.onErrorFlag = True    
+
+    def stopError(self):	 
+	self.onErrorFlag = False
+	#self.red.ChangeDutyCycle(100.0)
+	self.red.stop()
+	GPIO.cleanup(35)
+	#self.cleanAll()
+	
+    def onStart(self):
+	
+        self.green.ChangeDutyCycle(0.0)
+
+
+
+h = HardwareLedRpi()
+
+h.onError()
+time.sleep(4)
+h.onBusy()
+time.sleep(4)
+h.stopBusy()
+h.onStart()
+
+
+import atexit
+atexit.register(h.cleanAll)
+
+
+
